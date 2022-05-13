ea18b1309e898cfc4a31891edf65cb570ffc7919
GUSTAVO HENRIQUE DOS SANTOS FERNANDES <gustavo.hsfernandes3@senacsp.edu.br>
2022-05-13 09:44:49 -0300

    primeiro css style em react

--- terceiro/src/App.js
+++ terceiro/src/App.js
@@ -1,13 +1,13 @@
 function App() {
-  let n1 = Math.round(Math.random() * 200 - 100 + 5);
-  let n2 = Math.round(Math.random() * 200);
-
+  let styles = {
+    backgroundColor: "#f00",
+    color: "#fff",
+    display: "flex",
+    height: "100vh",
+  };
   return (
-    <div>
-      <p>
-        A soma entre {n1} e {n2} é {n1 + n2}
-      </p>
-      <p>O resultado acima é {(n1 + n2) % 2 === 0 ? "par" : "impar"}</p>
+    <div style={styles}>
+      <p>veja o background</p>
     </div>
   );
 }
