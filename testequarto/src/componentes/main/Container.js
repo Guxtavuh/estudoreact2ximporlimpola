5e3d3ce635eaaba3be4089169e48a309a69db08b
GUSTAVO HENRIQUE DOS SANTOS FERNANDES <gustavo.hsfernandes3@senacsp.edu.br>
2022-05-17 11:57:00 -0300

    api para loja em REACT, e css feito em partes

--- testequarto/src/componentes/main/Container.js
+++ testequarto/src/componentes/main/Container.js
@@ -1,11 +1,37 @@
+import { useState, useEffect } from "react";
 import Conteudo from "./Conteudo";
 import Messages from "./Messages";
 
 export default function Container() {
+  // Vamos iniciar o projeto lendo o url com dados da api
+  // Neste caso temos produtos para ler e montar a tabela com conteudo
+  // vamos usar o comando useState para iniciar o estado de dados
+  // dos produtos, Depois vou usar o comando useEffect para carregar os produtos que vem da api
+  // O comando useEffect é um comando de laço portanto é necessario fazer ele parar o laço
+  // quando terminar  de carregar os dados, Isso é feito usando colchetes ao final da instrução.
+  // para obter os dados iremos usar o comando fetch dentro do useEffect
+
+  const [produtos, setprodutos] = useState([
+    {
+      id: "",
+      nome: "",
+      categoria: "",
+      msg: [],
+    },
+  ]);
+
+  useEffect(() => {
+    fetch("http://10.26.44.26:5500/")
+      .then((response) => response.json())
+      .then((dados) => {
+        setprodutos(dados.output);
+      });
+  }, []);
+
   return (
     <div className="container">
-      <Messages />
-      <Conteudo />
+      <Messages dados={produtos} />
+      <Conteudo dados={produtos} />
     </div>
   );
 }
--- testequarto/src/componentes/main/Messages.js
+++ testequarto/src/componentes/main/Messages.js
@@ -1,8 +1,15 @@
-export default function Messages() {
+export default function Messages(props) {
   return (
     <div className="mensagens">
       <h2>Titulo da menssagem</h2>
-      <h3>Autor da menssagem</h3>
+      {props.dados.map((itens, ix) => {
+        <li key={ix}>
+          <h3>{itens.msg[ix].autor}</h3>
+          <br />
+          <p>{itens.msg[ix].mensagem}</p>
+        </li>;
+      })}
+      <h3>Autor</h3>
       <p>Menssagem</p>
     </div>
   );
